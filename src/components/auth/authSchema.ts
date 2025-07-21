dd2b1e9cd0ddefac18e07adf85c34c35835fc16b
votrongnhan <nhanvt@mor.com.vn>
2025-07-22 01:31:50 +0700

    config API and login page

--- src/components/auth/authSchema.ts
+++ src/components/auth/authSchema.ts
@@ -6,6 +6,6 @@ export interface SignInFormData {
 }
 
 export const signInSchema = yup.object({
-    // username: yup.string().email("Email không hợp lệ").required("Vui lòng nhập email"),
+    username: yup.string().email("Email không hợp lệ").required("Vui lòng nhập email"),
     password: yup.string().required("Vui lòng nhập mật khẩu"),
 });
